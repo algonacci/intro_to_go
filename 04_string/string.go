7320079d3501c3a446f69ae2f8d4792c072b2b83
algonacci <86970816+algonacci@users.noreply.github.com>
2021-09-28 23:43:22 +0700

    Add files via upload

---
+++ 04_string/string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Eric")
+	fmt.Println("Eric Julianto")
+	fmt.Println("Eric 'Algonacci' Julianto")
+
+	fmt.Println(len("Eric"))
+}
---
+++ 05_variable/variable.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Eric Julianto"
+	fmt.Println(name)
+
+	var pekerjaan = "Developer"
+	var umur = 20
+
+	fmt.Println("Pekerjaan: ", pekerjaan)
+	fmt.Println("Umur: ", umur)
+
+	semester := 5
+	country := "Indonesia"
+
+	fmt.Println("Semester kuliah: ", semester)
+	fmt.Println("Tinggal di: ", country)
+
+	var (
+		jurusan1 = "Hospitality"
+		jurusan2 = "Pariwisata"
+	)
+
+	fmt.Println("Jurusan Kuliah: ",jurusan1," dan ", jurusan2)
+
+	first,mid,last := "Universitas ", "Bunda ", "Mulia"
+	fmt.Println("Kuliah:", first, mid, last)
+}
